1e74f8004372ec636f66e3637da2204f038563bb
quentin <quentin.usiko@gmail.com>
2022-06-27 01:49:08 +0200

    fixe data links

--- src/routes/symbole-accessories/symbole-accessories.controller.ts
+++ src/routes/symbole-accessories/symbole-accessories.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { pathEnum } from 'src/models/enum';
-import { LocalAuthGuard } from 'src/services/auth/local-auth-guard';
+import { JwtAuthGuard } from 'src/services/auth/jwt-auth-guard';
 import { CtrlRoute } from '../class/controller.class';
 import { SymboleAccessoriesService } from './symbole-accessories.service';
 @Controller(pathEnum.SYMBOLEACCESSORIES)
@@ -9,25 +9,25 @@ export class SymboleAcessoriesController extends CtrlRoute<any> {
     super();
   }
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get()
   get() {
     return super.get();
   }
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   getItem(@Param() params) {
     return super.getItem(params);
   }
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post()
   Post(@Param() params) {
     super.post(params);
   }
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Put()
   Put(@Param() params) {
     super.put(params);
